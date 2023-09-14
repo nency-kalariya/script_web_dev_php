1b6cbc1958707fb19b2ba55f862fe557e50d41ea
nency-kalariya <144974985+nency-kalariya@users.noreply.github.com>
2023-09-14 08:03:56 +0530

    12.Develop a PHP program that uses echo to display an unordered list (<ul>) with three list items (<li>) containing different colors.

---
+++ script_12.php
@@ -0,0 +1,8 @@
+<?php
+	echo '<ul>';
+	echo '<li>' . "<font color=red>nency</font>" . '</li>' ;
+	echo '<li>' . "<font color=blue>krishna</font>" . '</li>' ;
+	echo '<li>' . "<font color=green>vishva</font>" . '</li>' ;
+	echo '</url>' ;
+	
+?>
